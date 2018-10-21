3d0d37cb96e142554c6cef3d08fdae58de09c4a5
MonoLogueChi <xxwhite.com@gmail.com>
2018-10-21 19:26:08 +0800

    升级依赖，修改webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanPlugin = require('clean-webpack-plugin');
 
-// 模板压缩
-// 详见：https://github.com/kangax/html-minifier#options-quick-reference
-
 var minifyHTML = {
   collapseInlineTagWhitespace: true,
   collapseWhitespace: true,
@@ -20,12 +17,11 @@ module.exports = {
     mobile: ["babel-polyfill", "./source-src/js/mobile.js"]
   },
   output: {
-    path: "./source",
-    publicPath: "./",
-    filename: "[name].[chunkhash:6].js"
+    path: __dirname+"/source",
+    filename: "[name].js"
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       loader: 'babel-loader?cacheDirectory',
       exclude: /node_modules/
@@ -34,32 +30,17 @@ module.exports = {
       loader: 'html'
     }, {
       test: /\.(scss|sass|css)$/,
-      loader: ExtractTextPlugin.extract('style-loader', ['css-loader?-autoprefixer', 'postcss-loader', 'sass-loader'])
+      loader: ExtractTextPlugin.extract({fallback:"style-loader",use:["css-loader","postcss-loader","sass-loader?outputStyle=compressed"]})
     }, {
       test: /\.(gif|jpg|png)\??.*$/,
       loader: 'url-loader?limit=500&name=img/[name].[ext]'
     }, {
       test: /\.(woff|svg|eot|ttf)\??.*$/,
-      loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
+      loader: "file-loader?name=fonts/[name].[ext]"
     }]
   },
-  alias: {
-    'vue$': 'vue/dist/vue.js'
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
-  // devtool: '#eval-source-map',
-  postcss: function() {
-    return [autoprefixer];
-  },
   plugins: [
-    new ExtractTextPlugin('[name].[chunkhash:6].css'),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
+    new ExtractTextPlugin('style.css'),
     new HtmlWebpackPlugin({
       inject: false,
       cache: false,
@@ -67,31 +48,10 @@ module.exports = {
       template: './source-src/script.ejs',
       filename: '../layout/_partial/script.ejs'
     }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      minify: minifyHTML,
-      template: './source-src/css.ejs',
-      filename: '../layout/_partial/css.ejs'
+    new CleanPlugin(['source/*.js'],{
+      verbose: true,
+      dry: false,
     })
   ],
   watch: true
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new CleanPlugin('builds')
-  ])
 }
