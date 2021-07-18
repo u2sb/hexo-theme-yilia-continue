b1167ce4fa9632857c0ff806b068d3ce089b6962
MonoLogue Chi <xxwhite.com@gmail.com>
2021-07-19 01:43:50 +0800

    更新依赖到最新

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const minifyHTML = {
   collapseWhitespace: true,
@@ -12,81 +11,116 @@ const minifyHTML = {
   removeStyleLinkTypeAttributes: true,
   useShortDoctype: true,
   collapseInlineTagWhitespace: true,
-  minifyJS: true,
-  minifyCSS: true
+  minifyJS: false,
+  minifyCSS: false,
 };
 
 module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false, //不将注释提取到单独的文件中
+      }),
+    ],
+  },
   entry: {
     main: "./source-src/js/main.js",
     slider: "./source-src/js/slider.js",
-    mobile: ["@babel/polyfill", "./source-src/js/mobile.js"]
+    mobile: ["@babel/polyfill", "./source-src/js/mobile.js"],
+    assetsLoader: {
+      import: "async-assets-loader",
+      library: {
+        name: "asyncAssetsLoader",
+        type: "umd",
+      },
+    },
   },
   output: {
     path: __dirname + "/source/assets/",
     publicPath: "./",
-    filename: "[name].[chunkhash:6].js"
+    filename: "[name].[chunkhash:6].js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader?cacheDirectory",
-        exclude: /node_modules/
+        use: {
+          loader: "babel-loader?cacheDirectory",
+        },
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        loader: "html"
+        use: {
+          loader: "html",
+        },
       },
       {
         test: /\.(scss|sass|css)$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            "css-loader",
-            "postcss-loader",
-            {
-              loader: "sass-loader",
-              options: {
-                implementation: require("sass"),
-                sassOptions: {
-                  outputStyle: "compressed"
-                }
-              }
-            }
-          ]
-        })
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                outputStyle: "compressed",
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(gif|jpg|png)\??.*$/,
-        loader: "url-loader?limit=500&name=img/[name].[ext]"
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name].[hash:6][ext]",
+        },
+        // use: {
+        //   loader: "url-loader?limit=500&name=img/[name].[ext]",
+        // },
       },
       {
         test: /\.(woff|svg|eot|ttf)\??.*$/,
-        loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
-      }
-    ]
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash:6][ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin({
       verbose: true,
-      dry: false
+      dry: false,
     }),
-    new ExtractTextPlugin("[name].[chunkhash:6].css"),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash:6].css",
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       cache: false,
       minify: minifyHTML,
       template: "./source-src/script.ejs",
-      filename: "../../layout/_partial/script.ejs"
+      filename: "../../layout/_partial/script.ejs",
     }),
     new HtmlWebpackPlugin({
       inject: false,
       cache: false,
       minify: minifyHTML,
       template: "./source-src/css.ejs",
-      filename: "../../layout/_partial/css.ejs"
-    })
+      filename: "../../layout/_partial/css.ejs",
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      cache: false,
+      minify: minifyHTML,
+      template: "./source-src/load-script.ejs",
+      filename: "../../layout/_partial/load-script.ejs",
+    }),
   ],
-  watch: false
+  watch: false,
 };
